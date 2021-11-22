ba62e0a232ea95bf07f2430c4bf9d650eab7d599
Emmanuel  Tassone <53381024+Emmatassone@users.noreply.github.com>
2021-11-22 13:48:07 -0300

    Update constants.py

--- Pulsar/constants.py
+++ Pulsar/constants.py
@@ -3,6 +3,6 @@ epsilon = np.array([[[0,0,0],[0,0,1],[0,-1,0]],[[0,0,-1],[0,0,0],[1,0,0]],[[0,1,
 
 c = 2.99792458e8
 G = 6.6725985e-11
-Msun = 1.98847e30     #Solar mass in CGS
+Msun = 1.98847e30   
 mu0=1.25663706212e-6 #Vacuum magnetic permeability in MKS: H/m = N/(A²m) 
     
