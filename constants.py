78087919a867496e846af9375d858531a41800fe
Emmanuel  Tassone <53381024+Emmatassone@users.noreply.github.com>
2021-11-27 14:23:10 -0300

    Add files via upload

--- constants.py
+++ constants.py
@@ -4,5 +4,5 @@ epsilon = np.array([[[0,0,0],[0,0,1],[0,-1,0]],[[0,0,-1],[0,0,0],[1,0,0]],[[0,1,
 c = 2.99792458e10
 G = 6.6725985e-8
 Msun = 1.98847e33     #Solar mass in CGS
-mu0=1.25663706212e-6 #Vacuum magnetic permeability in MKS: H/m = N/(A²m)  (No se usa por ahora)
-    
+mu0_mks=1.25663706212e-6 #Vacuum magnetic permeability in MKS= N/(A²).
+mu0=mu0_mks*1e5 #Vacuum magnetic permeability in CGS. 1N=10^5 dyne
